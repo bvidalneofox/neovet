a04be34ee41d3b2c62f0431ef9d3321ece9a0a16
bvidalneofox <v_andres01@hotmail.com>
2020-02-13 14:27:13 -0300

    metodos de login en back con token y proteccion de rutas

--- backVeterinaria/routes/api.php
+++ backVeterinaria/routes/api.php
@@ -13,41 +13,53 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//Login y Registro
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+
+//RUTAS PROTEGIDAS
+Route::group(['middleware' => 'auth.jwt'], function () {
+
+    //Rutas para los clientes
+    Route::post('setCliente', 'ClienteController@setCliente');
+    Route::get('getClientePorRut/{rut?}', 'ClienteController@getClientePorRut');
+    Route::get('getClientesPorNombre/{nombre?}', 'ClienteController@getClientesPorNombre');
+    Route::get('getAllClientes', 'ClienteController@getAllClientes');
+
+    //Rutas para las mascotas
+    Route::post('setMascota', 'MascotaController@setMascota');
+    Route::get('getMascotasClientePorRut/{rut?}', 'ClienteController@getMascotasClientePorRut');
+    Route::get('getMascotasPorNombre/{nombre?}', 'MascotaController@getMascotasPorNombre');
+    Route::get('getMascotaPorId/{id}', 'MascotaController@getMascotaPorId');
+    Route::get('getMascotasPorIdCliente/{id}', 'ClienteController@getMascotasPorIdCliente');
+
+    //Ruta para las consultas
+    Route::post('setConsulta', 'ConsultaController@setConsulta');
+    Route::get('getConsultasActivas', 'ConsultaController@getConsultasActivas');
+    Route::get('getConsultasInactivas', 'ConsultaController@getConsultasInactivas');
+    Route::post('setProcedimientoConsulta', 'ConsultaController@setProcedimientoConsulta');
+    Route::get('getProcedimientoConsulta/{id}', 'ConsultaController@getProcedimientoConsulta');
+    Route::post('setConsultaFinalizada', 'ConsultaController@setConsultaFinalizada');
+    Route::get('getConsultasInactivaPorIdMascota/{id}', 'ConsultaController@getConsultasInactivaPorIdMascota');
+
+    //Ruta para las vacunas
+    Route::post('setVacuna', 'HistorialVacunaController@setVacuna');
+    Route::get('getVacunasPorIdMascota/{id}', 'HistorialVacunaController@getVacunasPorIdMascota');
+
+    //Ruta para los eventos
+    Route::post('setEvento', 'EventoController@setEvento');
+    Route::get('getEventos', 'EventoController@getEventos');
+    Route::post('updateEvento', 'EventoController@updateEvento');
+    Route::post('deleteEvento', 'EventoController@deleteEvento');
+
+    //Ruta para las hospitalizaciones
+    Route::get('getHospitalizacionesActivas', 'HospitalizacionController@getHospitalizacionesActivas');
+
+
+    //DESLOGUEARSE
+    Route::get('logout', 'ApiController@logout');
 });
-
-//Rutas para los clientes
-Route::post('setCliente', 'ClienteController@setCliente');
-Route::get('getClientePorRut/{rut?}', 'ClienteController@getClientePorRut');
-Route::get('getClientesPorNombre/{nombre?}', 'ClienteController@getClientesPorNombre');
-Route::get('getAllClientes', 'ClienteController@getAllClientes');
-
-//Rutas para las mascotas
-Route::post('setMascota', 'MascotaController@setMascota');
-Route::get('getMascotasClientePorRut/{rut?}', 'ClienteController@getMascotasClientePorRut');
-Route::get('getMascotasPorNombre/{nombre?}', 'MascotaController@getMascotasPorNombre');
-Route::get('getMascotaPorId/{id}', 'MascotaController@getMascotaPorId');
-Route::get('getMascotasPorIdCliente/{id}', 'ClienteController@getMascotasPorIdCliente');
-
-//Ruta para las consultas
-Route::post('setConsulta', 'ConsultaController@setConsulta');
-Route::get('getConsultasActivas', 'ConsultaController@getConsultasActivas');
-Route::get('getConsultasInactivas', 'ConsultaController@getConsultasInactivas');
-Route::post('setProcedimientoConsulta', 'ConsultaController@setProcedimientoConsulta');
-Route::get('getProcedimientoConsulta/{id}', 'ConsultaController@getProcedimientoConsulta');
-Route::post('setConsultaFinalizada', 'ConsultaController@setConsultaFinalizada');
-Route::get('getConsultasInactivaPorIdMascota/{id}', 'ConsultaController@getConsultasInactivaPorIdMascota');
-
-//Ruta para las vacunas
-Route::post('setVacuna', 'HistorialVacunaController@setVacuna');
-Route::get('getVacunasPorIdMascota/{id}', 'HistorialVacunaController@getVacunasPorIdMascota');
-
-//Ruta para los eventos
-Route::post('setEvento', 'EventoController@setEvento');
-Route::get('getEventos', 'EventoController@getEventos');
-Route::post('updateEvento', 'EventoController@updateEvento');
-Route::post('deleteEvento', 'EventoController@deleteEvento');
-
-//Ruta para las hospitalizaciones
-Route::get('getHospitalizacionesActivas', 'HospitalizacionController@getHospitalizacionesActivas');
