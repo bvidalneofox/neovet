ccb3d836037135998b3dc57ff14d041e6b8c14a7
bvidalneofox <v_andres01@hotmail.com>
2020-03-10 13:07:29 -0300

    nueva configuracion archivos

--- backVeterinaria/app/Http/Controllers/ConfiguracionController.php
+++ backVeterinaria/app/Http/Controllers/ConfiguracionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Configuracion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ConfiguracionController extends Controller
 {
@@ -57,11 +58,17 @@ class ConfiguracionController extends Controller
         if ($validador['estado'] == 'success') {
             $configuracion = Configuracion::find('1');
             if (!is_null($configuracion)) {
-                $configuracion->ruta_imagen = $request->archivo;
-                if ($request->archivo != 'undefined') {
-                    $nombreImagen = $request->file('archivo')->getClientOriginalName();
-                    $request->file('archivo')->move('images', $nombreImagen);
-                    $configuracion->ruta_imagen = 'images/' . $nombreImagen;
+                // $configuracion->ruta_imagen = $request->archivo;
+                // if ($request->archivo != 'undefined') {
+                //     $nombreImagen = $request->file('archivo')->getClientOriginalName();
+                //     $request->file('archivo')->move('images', $nombreImagen);
+                //     $configuracion->ruta_imagen = 'images/' . $nombreImagen;
+                // }
+                $guardarArchivo = $this->guardarArchivo($request->archivo, 'ArchivosConfiguracion/');
+                if ($guardarArchivo['estado'] == "success") {
+                    $configuracion->ruta_imagen = $guardarArchivo['archivo'];
+                } else {
+                    return $guardarArchivo;
                 }
                 if ($configuracion->save()) {
                     return ['estado' => 'success', 'mensaje' => 'Logo Actualizado Correctamente.'];
@@ -97,5 +104,22 @@ class ConfiguracionController extends Controller
     public function getConfiguraciones()
     {
         return Configuracion::all()->first();
+    }
+
+    public function guardarArchivo($archivo, $ruta)
+    {
+        $filenameext = $archivo->getClientOriginalName();
+        $filename = pathinfo($filenameext, PATHINFO_FILENAME);
+        $extension = $archivo->getClientOriginalExtension();
+        $nombreArchivo = $filename . '_' . time() . '.' . $extension;
+        $rutaDB = 'storage/' . $ruta . $nombreArchivo;
+
+        $guardar = Storage::put($ruta . $nombreArchivo, (string) file_get_contents($archivo), 'public');
+
+        if ($guardar) {
+            return ['estado' =>  'success', 'archivo' => $rutaDB];
+        } else {
+            return ['estado' =>  'failed', 'mensaje' => 'Error al intentar guardar el archivo.'];
+        }
     }
 }
